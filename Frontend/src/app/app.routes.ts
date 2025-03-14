aaa775de1a81fa548c863401bd4edcd177b013e6
Luvss <camilopinzon2018@gmail.com>
2025-03-14 17:10:44 -0500

    Acerca

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './modules/home/login/login.component';
 import { ProductosComponent } from './modules/productos/productos.component';
 import { ContactComponent } from './modules/contact/contact.component';
 import { CartComponent } from './modules/cart/cart.component';
+import { AcercaComponent } from './modules/acerca/acerca.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'products', component: ProductosComponent },
+  { path: 'acerca', component: AcercaComponent },
   { path: 'cart', component: CartComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'home', component: HomeComponent }, // Página principal
