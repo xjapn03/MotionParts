edda9167479f6a7561e4d23c509e50aa0a89a8ce
mAtRiZ7 <je.escobar.castro@gmail.com>
2025-04-10 12:42:39 -0500

    login

---
+++ Frontend/src/app/modules/home/home.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { ProductosComponent } from '../productos/productos.component';
+
+export const routes: Routes = [
+    { path: 'products', component: ProductosComponent },
+
+];
